dcf7b2518d581030255461f57de90f33ea5242a0
borgesbrenno <142638992+borgesbrenno@users.noreply.github.com>
2023-11-24 15:59:25 -0300

    Add files via upload

---
+++ PWNB-SEMANA#08/JS.js
@@ -0,0 +1,3 @@
+const elementoLista = document.querySelector("ul"); // Usamos querySelector para buscar o elemento no HTML
+const elementoInput = document.querySelector("input");
+const elementoBotao = document.querySelector("button");
